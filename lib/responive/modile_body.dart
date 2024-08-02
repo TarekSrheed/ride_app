a1dfb00cf022703303baff71eeb039e5bde69cf8
hamzamohmmad99 <hamza.99.sh36@gmail.com>
2024-08-02 16:11:19 -0700

    add responsive

---
+++ lib/responive/modile_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ModileBody extends StatelessWidget {
+  const ModileBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
