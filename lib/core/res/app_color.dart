60e94753368dcc5456338228c2c4213531710d91
Tarek12334 <tareqsarheed@gmail.com>
2024-08-03 00:15:00 +0300

    add res for app

---
+++ lib/core/res/app_color.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color(0xff08B783);
+Color darkPrimaryColor = const Color(0xff008955);
+Color excellentColor = const Color(0xff00AA6D);
+Color iconDisplayColor = const Color(0xffB9E5D1);
+Color secondaryColor = const Color(0xffB7083C);
+Color warningColor = const Color(0xffEF6A01);
+Color offerTitleColor = const Color(0xffF57F17);
+Color cancelColor = const Color(0xffD32F2F);
+Color successColor = const Color(0xff43A048);
+Color white = Colors.white;
+
+
+
+Color titleColor = const Color(0xff414141);
+Color subTitleColor = const Color(0xffA0A0A0);
+Color subTilteTwoColor = const Color(0xffB8B8B8);
+Color deleteButtonColor = const Color(0xffF44336);
+Color hintColor = const Color(0xffD0D0D0);
+Color grayColor = const Color(0xff5A5A5A);
+Color blackColor = const Color(0xff121212);
+Color notificationTitleColor = const Color(0xff898989);
+Color appbarTitleColor = const Color(0xff2A2A2A);
---
+++ lib/core/res/app_style.dart
@@ -0,0 +1,211 @@
+import 'package:flutter/material.dart';
+import 'package:rideshare_app/core/res/app_color.dart';
+
+
+TextStyle titleStyle = TextStyle(
+  color: titleColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle subtitleStyle = TextStyle(
+  color: subTitleColor,
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+
+
+TextStyle buttonTextStyle = TextStyle(
+  color: white,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle titleFavoStyle = TextStyle(
+  color: titleColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+
+TextStyle skipForNowStyle = TextStyle(
+  color: subTilteTwoColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+
+TextStyle hintStyle = TextStyle(
+  color: hintColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle bySiginingStyle = TextStyle(
+  color: subTilteTwoColor,
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+TextStyle signUpWithStyle = TextStyle(
+  color: grayColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+TextStyle roleForSetPasswordStyle = TextStyle(
+  color: grayColor,
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle appbarTitleStyle = TextStyle(
+  color: appbarTitleColor,
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+);
+TextStyle specificationFeatureStyle = TextStyle(
+  color: grayColor,
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle appBarLeadingStyle = TextStyle(
+  color: appbarTitleColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle selectedNavigationBarStyle = TextStyle(
+  color: primaryColor,
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle unSelectedNavigationBarStyle = TextStyle(
+  color: titleColor,
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle todayYesterdayStyle = TextStyle(
+  color: blackColor,
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle notificationTitleStyle = TextStyle(
+  color: blackColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle notificationSubTitleStyle = TextStyle(
+  color: notificationTitleColor,
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+);
+TextStyle titleLocationScreenStyle = TextStyle(
+  color: grayColor,
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+);
+TextStyle itemStyle = TextStyle(
+  color: grayColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+TextStyle subItemStyle = TextStyle(
+  color: subTilteTwoColor,
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+TextStyle titleTwoStyle = TextStyle(
+  color: grayColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+TextStyle cycleFoundStyle = TextStyle(
+  color: subTilteTwoColor,
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+TextStyle buttonViewStyle = TextStyle(
+  color: darkPrimaryColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle yourBookingStyle = TextStyle(
+  color: grayColor,
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle thankYouStyle = TextStyle(
+  color: grayColor,
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle mustangStyle = TextStyle(
+  color: grayColor,
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle excellentStyle = TextStyle(
+  color: excellentColor,
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle enterOtherStyle = TextStyle(
+  color: primaryColor,
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle offerSubTitleAndDrowerItemStyle = TextStyle(
+  color: grayColor,
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle addMoneyStyle = TextStyle(
+  color: primaryColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle offerTitleStyle = TextStyle(
+  color: warningColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+TextStyle nameStyle = TextStyle(
+  color: grayColor,
+  fontSize: 28,
+  fontWeight: FontWeight.w500,
+);
+TextStyle emailStyle = TextStyle(
+  color: titleColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+TextStyle drowerNameStyle = TextStyle(
+  color: titleColor,
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle cancelStyle = TextStyle(
+  color: cancelColor,
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle aboutUsTitleStyle = TextStyle(
+  color: grayColor,
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
